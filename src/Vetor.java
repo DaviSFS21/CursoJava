e2f4e7da5c610e64328b6dc209b9431710913f2b
Davi Soares <davisfs2110@gmail.com>
2024-09-04 22:51:22 -0300

    feat: Inicia estudo de vetor

---
+++ src/Vetor.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Vetor {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantas alturas serão registradas?");
+        int n = sc.nextInt();
+
+        double[] alturas = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%nDigite a altura da pessoa %d: ", i + 1);
+            alturas[i] = sc.nextDouble();
+        }
+
+        double total = 0;
+        for (int i = 0; i < n; i++) {
+            total = alturas[i];
+        }
+        double avg = total / n;
+
+        System.out.printf("A média de altura é: %.2f%n", avg);
+    }
+}
